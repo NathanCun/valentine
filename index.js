cffda6a7b94662bf0d15366a711bcedeaf6fb1ae
Your Name <you@example.com>
2023-01-16 00:31:28 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const pageOne = document.getElementById('page-one');
+const pageTwo = document.getElementById('page-two');
+const pageThree = document.getElementById('page-three');
+const pageFour = document.getElementById('page-four');
+const pageFive = document.getElementById('page-five');
+function one() {
+    pageOne.style.display= 'none';
+}
+
+function two() {
+    pageTwo.style.display= 'none';
+}
+
+function three() {
+    pageThree.style.display= 'none';
+}
+
+function four() {
+    pageFour.style.display= 'none';
+}
+
+function five() {
+    pageFive.style.display= 'none';
+}
